adb5bee287aa9a1af53051901b6d0966c2869c02
Jayser Mendez <jayser34@hotmail.com>
2015-08-17 01:29:58 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+///Coded by Jayser M.
 namespace percentageCalculator
 {
     class Program
