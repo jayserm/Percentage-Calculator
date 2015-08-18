ebce97e475b7b3ef6a6d502042ae26b26b72864f
Jayser Mendez <jayser34@hotmail.com>
2015-08-18 02:26:43 -0400

    New Version Ignoring Lower and Upper Cases.
    Now you can input "y" or "n" with lower and upper cases.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-///Coded by Jayser M.
+
 namespace percentageCalculator
 {
     class Program
@@ -17,16 +17,16 @@ namespace percentageCalculator
             Console.WriteLine("Input the number in order to see the percent");
             a = Convert.ToInt32(Console.ReadLine()); ///here you will input the number you want to convert and it will be declared as an int.
             Console.WriteLine("this is your result " + a / x); ///there is the computation, dividing a (which is your number) by x (which is 100).
-            Console.WriteLine("Do you want to try another number? (Y/N) (Input Y for 'yes' and N for 'no' and then press enter)");
+            Console.WriteLine("Do you want to try another number? (Y/N) (Input Y for 'yes' and N for 'no')");
             string answer = Console.ReadLine();
             do
             {
-                if (answer.Equals("y"))
+                if (String.Compare(answer, "y", true) == 0)
                 {
                     Console.Clear();
                     goto retry;
                 }
-                else if (answer.Equals("n"))
+                else if (String.Compare(answer, "N", true) == 0)
                 {
                     Console.WriteLine("This app will close");
                     Console.WriteLine("Please, input any key to close it");
